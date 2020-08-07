05fd88f968f5b2dfb2fdd58db96396b5337fa55d
MohamedElkamary2 <amo263128@gmail.com>
2020-08-07 17:26:16 +0200

    Updates

---
+++ precache-manifest.6f840b79b93b0aea8979a34bdccd210e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "148ad13d946081ea9922eed761ed9390",
+    "url": "/Reminder/index.html"
+  },
+  {
+    "revision": "90e047312faa949aeca4",
+    "url": "/Reminder/static/css/2.1601c95f.chunk.css"
+  },
+  {
+    "revision": "9cd319c1f0499639a225",
+    "url": "/Reminder/static/css/main.2a3765f7.chunk.css"
+  },
+  {
+    "revision": "90e047312faa949aeca4",
+    "url": "/Reminder/static/js/2.abd76d8c.chunk.js"
+  },
+  {
+    "revision": "d2966845b94a3318bf32eecc7af8015d",
+    "url": "/Reminder/static/js/2.abd76d8c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9cd319c1f0499639a225",
+    "url": "/Reminder/static/js/main.5acd2697.chunk.js"
+  },
+  {
+    "revision": "77c112662432b4c01fce",
+    "url": "/Reminder/static/js/runtime-main.aec304a6.js"
+  }
+]);
